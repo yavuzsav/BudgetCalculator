f7c96b5986aa6e9623847a9ad238e0699fc5deae
Yavuz SAV <yavuzsav07@gmail.com>
2021-10-01 22:17:19 +0300

    Created IBaseRepository

---
+++ BudgetCalculator.DataAccess/Interfaces/IBaseRepository.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using BudgetCalculator.Entities.Abstract;
+
+namespace BudgetCalculator.DataAccess.Interfaces
+{
+    public interface IBaseRepository<T> where T : class, IEntity
+    {
+        T Add(T entity);
+        T Update(T entity);
+        void Delete(T delete);
+        IEnumerable<T> GetList(Expression<Func<T, bool>> expression = null);
+        Task<IEnumerable<T>> GetListAsync(Expression<Func<T, bool>> expression = null);
+        T Get(Expression<Func<T, bool>> expression = null);
+        Task<T> GetAsync(Expression<Func<T, bool>> expression = null);
+        int SaveChanges();
+        Task<int> SaveChangesAsync();
+        IQueryable<T> Query();
+        Task<int> Execute(FormattableString interpolatedQueryString);
+        int GetCount(Expression<Func<T, bool>> expression = null);
+        Task<int> GetCountAsync(Expression<Func<T, bool>> expression = null);
+    }
+}
