0cfb096512e47986fe65a365f0e2d6fbd88c086f
Yavuz SAV <yavuzsav07@gmail.com>
2021-10-01 21:40:34 +0300

    Created expense entity

---
+++ BudgetCalculator.Entities/Concrete/Expense.cs
@@ -0,0 +1,17 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using BudgetCalculator.Entities.Abstract;
+using Microsoft.EntityFrameworkCore;
+
+namespace BudgetCalculator.Entities.Concrete
+{
+    [Index(nameof(Id))]
+    public class Expense : IEntity
+    {
+        [Key] public Guid Id { get; set; }
+        [Column(TypeName = "decimal(12,2)")] public decimal Planned { get; set; }
+        [Column(TypeName = "decimal(12,2)")] public decimal Actual { get; set; }
+        [MaxLength(500)] public string Description { get; set; }
+    }
+}
